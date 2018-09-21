2b554142746b20aa72b1f5dc2fe6e7a76113d4af
Alejandro Sanchez <aalejo@gmail.com>
2018-09-21 17:12:10 +0000

    initial commit

---
+++ scripts/get_utils.js
@@ -0,0 +1,22 @@
+return (services, plugin) => new Promise(function(resolve, reject){
+    resolve({
+        log: function(...args){ if(plugin['breathecode'].debug) console.log(...args) },
+        notify: function(){
+            return {
+                error: (err=null) => {
+                    if(!err) err = 'Unspecified error';
+                    if(plugin['breathecode'].debug) console.error(err);
+                    services['dialog.error'].show("Error",err);
+                },
+                info: (msg=null) => {
+                    if(!msg) msg = 'Unspecified error';
+                    if(plugin['breathecode'].debug) console.log(msg);
+                    services['dialog.alert'].show("Success",msg);
+                }
+            };
+        },
+        openTab: function(url){
+            window.open(url);
+        }
+    });
+});
